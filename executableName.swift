693b26ca4c5f9ef4aa2dffda51535ca2bf2b2baa
Mathias Quintero <me@quintero.io>
2020-05-29 13:47:59 +0200

    Create executableName.swift

---
+++ executableName.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct Product: Codable {
+    let name: String
+    let type: [String : String?]
+}
+
+struct Package: Codable {
+    let products: [Product]
+}
+
+guard let data = ProcessInfo.processInfo.environment["JSON_PACKAGE"]?.data(using: .utf8) else {
+    fatalError("JSON Representation of Package was not delivered")
+}
+let decoded = try JSONDecoder().decode(Package.self, from: data)
+
+if decoded.products.count == 1 {
+    print(decoded.products[0].name)
+} else if let firstExecutable = decoded.products.first(where: { $0.type.keys.contains("executable") }) {
+    print(firstExecutable.name)
+} else {
+    fatalError("Failed to find proper executable name for package")
+}
