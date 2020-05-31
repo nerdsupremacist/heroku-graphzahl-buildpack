07aaf5f606b5776727a6a451530962ba12b2ea2b
Mathias Quintero <me@quintero.io>
2020-05-31 15:30:34 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ export PACKAGE_JSON=$(swift package dump-package)
 export EXECUTABLE_NAME=$(PACKAGE_JSON=$PACKAGE_JSON swift $(curl -fsSL https://raw.githubusercontent.com/nerdsupremacist/heroku-graphzahl-buildpack/master/executableName.swift))
 rm executableName.swift
 
-export EXECUTABLE_PATH=$(swift build -c release --show-bin-path)/$EXECUTABLE_NAME
+export EXECUTABLE_PATH=$(swift build -c debug --show-bin-path)/$EXECUTABLE_NAME
 echo $EXECUTABLE_PATH
 
 swift package clean
