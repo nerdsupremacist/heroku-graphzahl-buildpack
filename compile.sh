14a40d362394bce32a501b2a859e3b27b6e9d051
Mathias Quintero <me@quintero.io>
2020-05-31 15:30:00 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,12 +3,12 @@
 apt-get -y install libssl-dev
 apt-get -y install libz-dev
 
-curl https://raw.githubusercontent.com/nerdsupremacist/heroku-graphzahl-buildpack/master/executableName.swift > executableName.swift
 export PACKAGE_JSON=$(swift package dump-package)
-export EXECUTABLE_NAME=$(PACKAGE_JSON=$PACKAGE_JSON swift executableName.swift)
+export EXECUTABLE_NAME=$(PACKAGE_JSON=$PACKAGE_JSON swift $(curl -fsSL https://raw.githubusercontent.com/nerdsupremacist/heroku-graphzahl-buildpack/master/executableName.swift))
 rm executableName.swift
+
+export EXECUTABLE_PATH=$(swift build -c release --show-bin-path)/$EXECUTABLE_NAME
 
 swift package clean
 swift build -c debug -Xlinker -E
 
-export EXECUTABLE_PATH=./.build/debug$EXECUTABLE_NAME
