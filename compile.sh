4882282bea7b4a94400222ca2583b8d7eec09f9d
Mathias Quintero <me@quintero.io>
2020-05-31 15:43:21 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+echo "--> Installing required packages"
 apt-get -y install libssl-dev
 apt-get -y install libz-dev
 
+echo "--> Detecting Namge of Executable"
 export PACKAGE_JSON=$(swift package dump-package)
 export EXECUTABLE_NAME=$(curl -fsSL https://raw.githubusercontent.com/nerdsupremacist/heroku-graphzahl-buildpack/master/executableName.swift | swift -)
+echo "--> Detected executable $EXECUTABLE_NAME"
 
-export EXECUTABLE_PATH=$(swift build -c debug --show-bin-path)/$EXECUTABLE_NAME
-echo $EXECUTABLE_PATH
-
+echo "--> Compiling Application"
 swift package clean
 swift build -c debug -Xlinker -E
 
+export EXECUTABLE_PATH=$(swift build -c debug --show-bin-path)/$EXECUTABLE_NAME
+echo "--> Will run application from $EXECUTABLE_PATH"
