64f0d824dfb6cd674f22d862ba0c5c6314e4c3ef
Mathias Quintero <me@quintero.io>
2020-05-31 16:17:25 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -7,13 +7,13 @@ apt-get -y install libz-dev
 echo " ---> Detecting Name of Executable"
 export PACKAGE_JSON=$(swift package dump-package)
 export EXECUTABLE_NAME=$(curl -fsSL https://raw.githubusercontent.com/nerdsupremacist/heroku-graphzahl-buildpack/master/executableName.swift | swift -)
-echo " ---> Detected executable: `$EXECUTABLE_NAME`"
+echo " ---> Detected executable: $EXECUTABLE_NAME"
 
-# echo " ---> Compiling Application"
-# swift package clean
-# swift build -c debug -Xlinker -E
+echo " ---> Compiling Application"
+swift package clean
+swift build -c debug -Xlinker -E
 
 export EXECUTABLE_PATH=$(swift build -c debug --show-bin-path)/$EXECUTABLE_NAME
-echo " ---> Will run application from: `$EXECUTABLE_PATH`"
+echo " ---> Will run application from: $EXECUTABLE_PATH"
 
 ln -s $EXECUTABLE_PATH compiled
